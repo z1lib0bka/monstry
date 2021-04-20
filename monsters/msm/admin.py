9664c5f0d8827a8f5b51e4bc53f58fb551a507c0
Tesorp1X <rakteam2014@gmail.com>
2021-04-20 19:34:16 +0700

    Monster model update: new fields default_time and advanced_time represented as DurationField. Added custom form to represent monster object correctly (due to usage of DurationFields in model). Initial time values has been moved to settings.py as INITIAL_DEFAULT_TIME and INITIAL_ADVANCED_TIME. A little update in admin.py: adding custom form generation for MonsterModelAdmin objects.

--- monsters/msm/admin.py
+++ monsters/msm/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 
-from .models import *
+from .models import Monster, Island
+from .forms import MonsterForm
 
 
 @admin.register(Monster)
 class MonsterModelAdmin(admin.ModelAdmin):
-    list_display = ['name']
+    list_display = ['name', 'default_time', 'advanced_time']
 
     ordering = ['name']
+
+    form = MonsterForm
 
 
 @admin.register(Island)
--- monsters/msm/models.py
+++ monsters/msm/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 
 class Monster(models.Model):
     name = models.CharField(max_length=32, verbose_name='Monster name')
 
     # image = models.ImageField()
-    # convert ot many to many, different islands different strategy
+    # convert to many to many, different islands different strategy
     # how_to_breed = models.CharField(max_length=512, verbose_name='Breed Strategy')
 
-    # time = models.CharField(max_length=32, verbose_name='Time')
+    default_time = models.DurationField(verbose_name='Default Time to breed',
+                                        default=settings.INITIAL_DEFAULT_TIME)
+
+    advanced_time = models.DurationField(verbose_name='Advanced Time to breed',
+                                         default=settings.INITIAL_ADVANCED_TIME)
 
     buy_cost = models.IntegerField(verbose_name='Buy cost')
 
